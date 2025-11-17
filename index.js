a96120599d543fe0a42bd7e10f875f1b17b52f0a
DevelopWebAdnan <adnaniqbal023@yahoo.com>
2025-11-17 11:51:39 +0600

    View all coffee and create Coffee Card.

--- index.js
+++ index.js
@@ -28,9 +28,11 @@ async function run() {
 
     const coffeeCollection = client.db('coffeeDB').collection('coffee');
 
-    // app.get('/coffee', async(req, res) => {
-    //   const cursor = 
-    // })
+    app.get('/coffee', async(req, res) => {
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
 
     app.post('/coffee', async(req, res) => {
       const newCoffee = req.body;
